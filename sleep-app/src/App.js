bc983a0b66999200ad0f1bf4be96406bbdad76d1
Roland J Jarvis <Rjarvis87@gmail.com>
2019-10-17 23:26:43 -0400

    TimerClock Layout Started

--- sleep-app/src/App.js
+++ sleep-app/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login.js';
-import Signup from './components/Signup';
-import SleepTracking from './components/SleepTracking';
-import SleepRating from './components/SleepRating';
-import SleepResults from './components/SleepResults';
+import TimerClock from './components/TimerClock';
 
 function App() {
+  
   return (
     // <Login />
     // <Signup />
     // <SleepTracking />
     // <SleepRating />
-    <SleepResults />
+    // <SleepResults />
+    <TimerClock/>
   );
 }
 
